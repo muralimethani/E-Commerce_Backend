bbd3304999daeaf3dbf0ce7a54d581930ae42ed3
Murali <m.muralibtech496@gmail.com>
2023-12-14 11:59:35 +0530

    added Cart Check Out API

---
+++ src/main/java/com/example/ECommerceBackend/dto/RequestDto/CheckOutCartRequestDto.java
@@ -0,0 +1,2 @@
+package com.example.ECommerceBackend.dto.RequestDto;public class CheckOutCartRequestDto {
+}
---
+++ src/main/java/com/example/ECommerceBackend/repository/OrderedRepository.java
@@ -0,0 +1,2 @@
+package com.example.ECommerceBackend.repository;public interface OrderedRepository {
+}
